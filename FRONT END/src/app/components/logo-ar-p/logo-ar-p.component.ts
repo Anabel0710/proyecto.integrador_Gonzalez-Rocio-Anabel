0906109e422ae6d10e053abe55f17865dd151f18
Rocío Anabel <rocioanabelgonzalez07@gmail.com>
2023-03-24 17:52:42 -0300

    Creacion de carpeta Front end
    se creo una carpeta y se movieron los archivos

---
+++ FRONT END/src/app/components/logo-ar-p/logo-ar-p.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-ar-p',
+  templateUrl: './logo-ar-p.component.html',
+  styleUrls: ['./logo-ar-p.component.css']
+})
+export class LogoArPComponent {
+
+}
